5d731e111848ea2aafa68118262b8de350e4bc13
Michal Witkowski <batoponton@gmail.com>
2018-08-20 14:06:18 +0200

    Added index for importing common components

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 const CardSection = (props) => {
     return (
         <View style={styles.containerStyle}>
-        {props.children}
+            {props.children}
         </View>
     );
 };
@@ -22,4 +22,4 @@ const styles = {
     }
 };
 
-export default CardSection;
+export { CardSection };
