cddd2de39b03ce477d7950561f021475ca11b953
Michal Witkowski <batoponton@gmail.com>
2018-08-28 14:45:22 +0200

    created Input component

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import {Text, View, TextInput} from 'react-native';
 
-const Input = ({label}) => {
+const Input = ({label, value, onChangeText}) => {
+  const {inputStyle, labelStyle, containerStyle} = styles;
 
   return (
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
-      style={{height: 20, width: 100}}
+        value={value}
+        onChangeText={onChangeText}
+        style={inputStyle}
       />
     </View>
   );
+};
+
+const styles = {
+  inputStyle: {
+    color: '#000',
+    paddingRight: 5,
+    paddingLeft: 5,
+    fontSize: 18,
+    lineHeight: 23,
+    flex: 2
+  },
+  labelStyle: {
+    fontSize: 18,
+    paddingLeft: 20,
+    flex: 1
+  },
+  containerStyle: {
+    display: 'flex',
+    flex: 1,
+    height: 40,
+    flexDirection: 'row',
+    align: 'center'
+  }
 };
 
 
