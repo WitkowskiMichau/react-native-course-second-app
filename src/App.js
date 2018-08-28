f3aebf1cab37cb2e3bbbf057fbd5b1df83bac2f6
Michal Witkowski <batoponton@gmail.com>
2018-08-28 09:56:20 +0200

    Added login form and text input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDJoZ_F-0SH5JPqXwluZsOq5dfl-k_mEWM',
+            authDomain: 'reactnative-auth-df00c.firebaseapp.com',
+            databaseURL: 'https://reactnative-auth-df00c.firebaseio.com',
+            projectId: 'reactnative-auth-df00c',
+            storageBucket: 'reactnative-auth-df00c.appspot.com',
+            messagingSenderId: '954679390309'
+        })
+    }
+
     render() {
         return (
             <View>
-            <Text>something</Text>
+                <Header headerText="Auth"></Header>
+                <LoginForm />
             </View>
         );
     }
-} 
+}
 
 export default App;
